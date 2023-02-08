4bab3e51d770bd6b0c40ef3c5fcc98e33d92f892
BriekKeters <55976763+BriekKeters@users.noreply.github.com>
2023-02-08 14:56:16 +0100

    :fire: Persons api

---
+++ server/src/TickTick/TickTick.Api/Controllers/PersonsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TickTick.Models;
+
+namespace TickTick.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult get()
+        {
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < 5; i++) 
+            {
+                Person person = new Person("Briek", "Keters", "briek.keters@gmail.com") { Id = (ulong)i};
+                people.Add(person);
+            }
+            return Ok(people);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult get(Guid id)
+        {
+            //TODO: haal een persoon op
+            Person person = new Person("Briek", "Keters", "briek.keters@gmail.com");
+            return Ok(person);
+        }
+    }
+}
